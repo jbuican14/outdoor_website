87feb3bb0c2d9d837290b2db28f2c5916b3f1e3a
Bew <Bew@sysops-MBP.broadband>
2021-01-21 14:07:35 +0000

    Initail commit adding header section to the page

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import logoWhite from '../img/logo-white.png';
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src={logoWhite} alt="Logo White" className="logo" />
+      </div>
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+
+        <button name="Discover More" className="btn btn-white btn-animated">
+          Discover our tours
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
