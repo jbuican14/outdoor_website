08dd0ff97319430a520a81266be297f1482dce6c
Bew <Bew@sysops-MBP.broadband>
2021-01-22 23:39:24 +0000

    converting CSS and HTML to BEM style guide

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import logoWhite from '../img/logo-white.png';
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={logoWhite} alt="Logo White" className="logo" />
+      <div className="header__logo-box">
+        <img src={logoWhite} alt="Logo White" className="header__logo" />
       </div>
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
 
-        <button name="Discover More" className="btn btn-white btn-animated">
+        <button name="Discover More" className="btn btn--white btn--animated">
           Discover our tours
         </button>
       </div>
