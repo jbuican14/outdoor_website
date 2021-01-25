5170719670b0fc99b2cbe1008c6d92d3f1112742
Bew <Bew@sysops-MBP.broadband>
2021-01-25 23:53:26 +0000

    build grid layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './sass/index.scss';
 
 import Header from './components/Header';
+import CustomGrid from './components/CustomGrid'; 
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CustomGrid />
     </div>
   );
 }
