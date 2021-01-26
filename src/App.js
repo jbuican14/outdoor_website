38708e78fa901d3f24621d91ec620315f270bccf
Bew <Bew@sysops-MBP.broadband>
2021-01-26 10:03:42 +0000

    Add first part of the About page (layout grid 1 of 2 )

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './sass/index.scss';
 
+// import CustomGrid from './components/CustomGrid';
 import Header from './components/Header';
-import CustomGrid from './components/CustomGrid'; 
-
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CustomGrid />
+      <MainPage />
+      {/* <CustomGrid /> */}
     </div>
   );
 }
