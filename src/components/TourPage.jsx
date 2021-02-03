088cdf8f3817311a64e9472d4d62733b68e6fe8d
Bew <Bew@sysops-MBP.broadband>
2021-02-03 11:47:28 +0000

    Adding card animation style [color] and adding back side when the card is flipped

--- src/components/TourPage.jsx
+++ src/components/TourPage.jsx
@@ -11,19 +11,25 @@ const TourPage = () => {
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">Card FRONT</div>
-            <div className="card__side card__side--back">Card BACK !!! </div>
+            <div className="card__side card__side--back card__side--back-1">
+              Card BACK !!!
+            </div>
           </div>
         </div>
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">Card FRONT</div>
-            <div className="card__side card__side--back">Card BACK !!!</div>
+            <div className="card__side card__side--back card__side--back-1">
+              Card BACK !!!
+            </div>
           </div>
         </div>
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">Card FRONT</div>
-            <div className="card__side card__side--back">Card BACK !!!</div>
+            <div className="card__side card__side--back card__side--back-1">
+              Card BACK !!!
+            </div>
           </div>
         </div>
       </div>
